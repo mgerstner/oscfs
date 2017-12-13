d4c24e3e4289770e6740e2d4d44dfe60e515d757
Matthias Gerstner <matthias.gerstner@suse.de>
2017-12-13 11:48:33 +0100

    module init: fixed and improved error handling
    - protection against the wrong fuse python module being present.
    - correctly import print(), sys

--- oscfs/__init__.py
+++ oscfs/__init__.py
@@ -1,14 +1,24 @@
+from __future__ import with_statement, print_function
+import sys
+
 try:
-	# TODO: make a sanity check whether the this is really fusepy and not
-	# python-fuse
 	import fuse
+	# make sure we don't have the python-fuse module which uses a similar
+	# but incompatible API
+	# python-fuse has the APIVersion attribute which fusepy doesn't have.
+	api_version = getattr(fuse, "APIVersion", None)
+	if api_version != None:
+		print("Wrong python fuse module is installed", file = sys.stderr)
+		print("oscfs required the fusepy module, this is the python-fuse module", file = sys.stderr)
+		sys.exit(1)
+
 except ImportError:
-	print("Failed to import the python fuse module.")
+	print("Failed to import the python fuse module.", file = sys.stderr)
 	sys.exit(1)
 
 try:
 	import osc
 except ImportError:
-	print("Failed to import the python osc module.")
+	print("Failed to import the python osc module.", file = sys.stderr)
 	sys.exit(1)
 
