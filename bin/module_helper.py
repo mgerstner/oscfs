481d618917af6ac3ceed0d6884b487172d976ca4
Matthias Gerstner <matthias.gerstner@suse.de>
2022-07-25 12:01:06 +0200

    module_helper: insert local modules directory at the front of the path
    this prevents system wide or user wide concurring oscfs modules to be
    loaded, leading to surprises and possibly inconcistencies.

--- bin/module_helper.py
+++ bin/module_helper.py
@@ -9,7 +9,7 @@ def tryFindModule(module):
     """Adds .. to the current module path, tries to import $module and exits if it is not found."""
     # get the full path of the parent directory and append the name
     parent_dir = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
-    sys.path.append(parent_dir)
+    sys.path.insert(0, parent_dir)
 
     # check if the module can be loaded now
     if pkgutil.find_loader(module) is None:
