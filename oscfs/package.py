mgerstner/oscfs

commit cd692a1d2f9fabe4ac5451832c3b7df109302c07

global: fix Flake8 findings

oscfs/package.py
@@ -1,16 +1,16 @@
-# local modules
 import oscfs.types
 import oscfs.obs
 import oscfs.obsfile
 import oscfs.link
 import oscfs.refreshtrigger
 
+
 class Package(oscfs.types.DirNode):
     """This type represents a package node of the file system containing
     all package files and metadata as files.
     """
 
-    def __init__(self, parent, name, project, package, revision = None):
+    def __init__(self, parent, name, project, package, revision=None):
 
         super(Package, self).__init__(parent, name)
         self.m_revision = revision
@@ -62,12 +62,12 @@ class Package(oscfs.types.DirNode):
 
         for ft, name, size, mtime, target in obs.getPackageFileList(
             self.getProject().getName(), self.m_package,
-            revision = self.m_revision
+            revision=self.m_revision
         ):
             if ft == types.regular:
                 node = oscfs.obsfile.ObsFile(
                     self, name, size, mtime,
-                    revision = self.m_revision
+                    revision=self.m_revision
                 )
             elif ft == types.symlink:
                 node = oscfs.link.Link(self, name, target)
@@ -97,6 +97,7 @@ class Package(oscfs.types.DirNode):
             # only add the API dir for the current version of the
             # package, the pkg meta data is not versioned.
             self._addApiDir()
+
 
 class PkgApiDir(oscfs.types.DirNode):
     """This type provides access to additional meta data for a package.
@@ -179,12 +180,8 @@ class PkgApiDir(oscfs.types.DirNode):
         devel_proj = self.getPkgInfo().getDevelProject()
         if devel_proj:
             devel_link = "develproject"
-            target = "{}/{}".format(
-                    devel_proj,
-                    self.getPackage().getName()
-            )
-            self.m_entries[devel_link] = \
-                oscfs.link.Link(self, devel_link, target)
+            target = f"{devel_proj}/{self.getPackage().getName()}"
+            self.m_entries[devel_link] = oscfs.link.Link(self, devel_link, target)
 
         prj_info = self.getProject().getApiDir().getPrjInfo()
         incident = self.getPkgInfo().getMaintenanceIncident(prj_info)
@@ -194,8 +191,8 @@ class PkgApiDir(oscfs.types.DirNode):
             # package was built
             incident_link = "incident"
             target = incident
-            self.m_entries[incident_link] = \
-                oscfs.link.Link(self, incident_link, target)
+            self.m_entries[incident_link] = oscfs.link.Link(self, incident_link, target)
+
 
 class CommitsDir(oscfs.types.DirNode):
     """This types provides access to each individual commit for a
@@ -207,14 +204,14 @@ class CommitsDir(oscfs.types.DirNode):
         self.m_package = package
 
     def update(self):
-        package = self.m_package
         infos = self.m_parent.getCachedCommitInfos()
 
         for rev in range(len(infos)):
             info = infos[rev]
 
-            commit = "{}".format(rev+1)
+            commit = "{rev + 1}"
             self.m_entries[commit] = CommitNode(self, info, commit)
+
 
 class RevisionsDir(oscfs.types.DirNode):
     """This type provides access to all revisions that exist for a
@@ -227,16 +224,15 @@ class RevisionsDir(oscfs.types.DirNode):
         self.m_package = package
 
     def update(self):
-        package = self.m_package
-
         for info in self.m_parent.getCachedCommitInfos():
             name = str(info.getRevision())
             self.m_entries[name] = Package(
                 self, name,
-                project = self.getProject().getName(),
-                package = self.getPackage().getName(),
-                revision = info.getRevision()
+                project=self.getProject().getName(),
+                package=self.getPackage().getName(),
+                revision=info.getRevision()
             )
+
 
 class LogNode(oscfs.types.FileNode):
     """This node type contains the commit log for the package it resides
@@ -262,6 +258,7 @@ class LogNode(oscfs.types.FileNode):
 
         return log
 
+
 class NumRevisionsNode(oscfs.types.FileNode):
     """This node type contains the number of commits for the package it
     resides in."""
@@ -277,8 +274,6 @@ class NumRevisionsNode(oscfs.types.FileNode):
         self.setContent(self.m_revisions)
 
     def fetchRevisions(self):
-
-        package = self.m_package
         infos = self.m_parent.getCachedCommitInfos()
 
         return str(len(infos))
@@ -305,16 +300,17 @@ class CommitNode(oscfs.types.FileNode):
     def buildCommit(self):
 
         ret = "revision {r} | {user} | {date} | {req}\n".format(
-            r = self.m_info.getRevision(),
-            user = self.m_info.getAuthor(),
-            date = self.m_info.getDate().strftime("%X %x"),
-            req = self.m_info.getReqId()
+            r=self.m_info.getRevision(),
+            user=self.m_info.getAuthor(),
+            date=self.m_info.getDate().strftime("%X %x"),
+            req=self.m_info.getReqId()
         )
 
         ret += "-" * (len(ret) - 1)
         ret += "\n"
         ret += self.m_info.getMessage()
         return ret
+
 
 class RequestNode(oscfs.types.FileNode):
     """This node contains the specific request info for a request."""
@@ -341,6 +337,7 @@ class RequestNode(oscfs.types.FileNode):
         )
         return dt
 
+
 class RequestsDir(oscfs.types.DirNode):
     """This type provides access to all requests that exist for a
     package. It allows to inspect individual requests directly."""
@@ -354,11 +351,12 @@ class RequestsDir(oscfs.types.DirNode):
         obs = self.getRoot().getObs()
 
         for req in obs.getPackageRequestList(
-                project = self.getProject().getName(),
-                package = self.getPackage().getName()
+                project=self.getProject().getName(),
+                package=self.getPackage().getName()
         ):
             label = "{}:{}".format(req.reqid, req.state.name)
             self.m_entries[label] = RequestNode(self, req, label)
+
 
 class MetaNode(oscfs.types.FileNode):
     """This node type contains the raw XML metadata of a package."""
@@ -371,6 +369,7 @@ class MetaNode(oscfs.types.FileNode):
     def fetchContent(self):
         meta = self.m_parent.getPkgMeta()
         self.setContent(meta)
+
 
 class DescriptionNode(oscfs.types.FileNode):
     """This node returns a formatted description of the package."""
@@ -399,6 +398,7 @@ class DescriptionNode(oscfs.types.FileNode):
         )
         self.setContent(desc)
 
+
 class MaintainersNode(oscfs.types.FileNode):
     """This node returns a list of maintainers for the package."""
 
@@ -412,6 +412,7 @@ class MaintainersNode(oscfs.types.FileNode):
         maintainers = '\n'.join(pkg_info.getMaintainers())
         self.setContent(maintainers)
 
+
 class BugownersNode(oscfs.types.FileNode):
     """This node returns a list of bugownders for the package."""
 
@@ -424,6 +425,7 @@ class BugownersNode(oscfs.types.FileNode):
         pkg_info = self.m_parent.getPkgInfo()
         bugowners = '\n'.join(pkg_info.getBugowners())
         self.setContent(bugowners)
+
 
 class BuildresultsNode(oscfs.types.FileNode):
     """This node returns a the current build results for configured
@@ -445,6 +447,7 @@ class BuildresultsNode(oscfs.types.FileNode):
 
         content = results.getTable()
         self.setContent(content)
+
 
 class BuildlogNode(oscfs.types.FileNode):
     """This type returns a certain build log for a certain package upon
@@ -486,6 +489,7 @@ class BuildlogNode(oscfs.types.FileNode):
         log = obs.getBuildlog(*args)
         self.setContent(log)
 
+
 class BuildlogsDir(oscfs.types.DirNode):
     """This type provides access to a repository/arch hierarchy that
     allows access to the current build logs of a package."""
@@ -514,6 +518,7 @@ class BuildlogsDir(oscfs.types.DirNode):
                 repo,
                 arch
             )
+
 
 class BinaryFileNode(oscfs.types.FileNode):
     """This type returns a certain binary artifact's data upon read."""
@@ -545,6 +550,7 @@ class BinaryFileNode(oscfs.types.FileNode):
         # caching.
         content = obs.getBinaryFileContent(*args)
         self.setContent(content)
+
 
 class BinariesDir(oscfs.types.DirNode):
     """This type provides access to a repository/arch hierarchy that
@@ -594,4 +600,3 @@ class BinariesDir(oscfs.types.DirNode):
                     arch,
                     binary
                 )
-

oscfs/types.py
@@ -11,14 +11,14 @@ import fuse
 # local modules
 import oscfs.misc
 
-class FileType(object):
 
+class FileType(object):
     regular = 1
     directory = 2
     symlink = 4
 
-class Stat(object):
 
+class Stat(object):
     start_time = datetime.datetime.now()
 
     def __init__(self):
@@ -94,6 +94,7 @@ class Stat(object):
     def isWriteable(self):
         return (self.st_mode & stat.S_IWUSR) != 0
 
+
 class Node(object):
     """Generic file node type which needs to be specialized for regular
     files, directories et al.
@@ -104,9 +105,9 @@ class Node(object):
     Also the Stat information is kept in this base class.
     """
 
-    max_cache_time = datetime.timedelta(minutes = 60)
+    max_cache_time = datetime.timedelta(minutes=60)
 
-    def __init__(self, parent, name, _type = FileType.regular):
+    def __init__(self, parent, name, _type=FileType.regular):
 
         self.m_stat = Stat()
         self.m_name = name
@@ -117,7 +118,7 @@ class Node(object):
 
     @classmethod
     def setMaxCacheTime(cls, seconds):
-        cls.max_cache_time = datetime.timedelta(seconds = seconds)
+        cls.max_cache_time = datetime.timedelta(seconds=seconds)
 
     def getStat(self):
         return self.m_stat
@@ -136,7 +137,7 @@ class Node(object):
         return age > Node.max_cache_time
 
     def wasEverUpdated(self):
-        return self.m_last_updated != None
+        return self.m_last_updated is not None
 
     def setCacheFresh(self):
         self.m_last_updated = datetime.datetime.now()
@@ -173,7 +174,7 @@ class Node(object):
     def isRoot(self):
         """Returns whether this node is the root node of the file
         system."""
-        return self.m_parent == None
+        return self.m_parent is not None
 
     def getRoot(self):
         """Returns the root node of the file system."""
@@ -195,8 +196,10 @@ class Node(object):
         """Returns the package node the current node belongs to."""
         import oscfs.package
 
-        ret = self if isinstance(self, oscfs.package.Package) else \
-                self._findParent(oscfs.package.Package)
+        if isinstance(self, oscfs.package.Package):
+            ret = self
+        else:
+            ret = self._findParent(oscfs.package.Package)
 
         while True:
             # in case of nested paths like
@@ -224,10 +227,10 @@ class Node(object):
                 print("Failed to update {}: {}".format(
                     self.getName(),
                     oscfs.misc.getFriendlyException(e),
-                    file = sys.stderr
-                ))
+                ), file=sys.stderr)
                 raise fuse.FuseOSError(errno.EFAULT)
             self.setCacheFresh()
+
 
 class FileNode(Node):
     """Specialized Node type for read-only regular files. Implementations
@@ -242,7 +245,7 @@ class FileNode(Node):
         self.m_content = None
         self.m_use_cache = True
 
-    def setContent(self, content, date = None):
+    def setContent(self, content, date=None):
         if isinstance(content, str):
             content = content.encode('utf8')
         self.m_content = content
@@ -262,9 +265,10 @@ class FileNode(Node):
         if self.m_content is None or not self.m_use_cache:
             self.fetchContent()
 
-        ret = self.m_content[offset:offset+length]
+        ret = self.m_content[offset:offset + length]
 
         return ret
+
 
 class TriggerNode(Node):
     """Specialized Node type for writable pseudo files. It expects a
@@ -300,6 +304,7 @@ class TriggerNode(Node):
 
         return len(data)
 
+
 class DirNode(Node):
     """Specialized Node type for directories. This type introduces a
     dictionary of name -> Node mappings."""
@@ -309,7 +314,7 @@ class DirNode(Node):
         super(DirNode, self).__init__(
             parent,
             name,
-            _type = FileType.directory
+            _type=FileType.directory
         )
         # correct link count for directories
         self.getStat().setLinks(2)
@@ -339,6 +344,7 @@ class DirNode(Node):
         for entry in self.m_entries.values():
             entry.setCacheStale()
 
+
 class PlainDirNode(DirNode):
     """Specialized DirNode that doesn't implement its own update logic.
     This type of dir can be used to implement subdirs that shouldn't act
@@ -351,4 +357,3 @@ class PlainDirNode(DirNode):
 
     def update(self):
         pass
-

bin/module_helper.py
@@ -16,4 +16,5 @@ def tryFindModule(module):
         print("The module %s could not be found in '%s'!" % (module, parent_dir))
         sys.exit(4)
 
+
 tryFindModule("oscfs")

bin/oscfs
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 try:
-    import module_helper
+    import module_helper  # noqa: F401
 except ImportError:
     # if oscfs is normally installed then this is okay
     pass
@@ -10,4 +10,3 @@ import oscfs.misc
 
 the_fs = oscfs.fs.OscFs()
 the_fs.run()
-

oscfs/__init__.py
@@ -6,18 +6,17 @@ try:
     # but incompatible API
     # python-fuse has the APIVersion attribute which fusepy doesn't have.
     api_version = getattr(fuse, "APIVersion", None)
-    if api_version != None:
-        print("Wrong python fuse module is installed", file = sys.stderr)
-        print("oscfs requires the fusepy module, this is the python-fuse module", file = sys.stderr)
+    if api_version is not None:
+        print("Wrong python fuse module is installed", file=sys.stderr)
+        print("oscfs requires the fusepy module, this is the python-fuse module", file=sys.stderr)
         sys.exit(1)
 
 except ImportError:
-    print("Failed to import the python fuse module.", file = sys.stderr)
+    print("Failed to import the python fuse module.", file=sys.stderr)
     sys.exit(1)
 
 try:
-    import osc
+    import osc  # noqa: F401
 except ImportError:
-    print("Failed to import the python osc module.", file = sys.stderr)
+    print("Failed to import the python osc module.", file=sys.stderr)
     sys.exit(1)
-

oscfs/fs.py
@@ -1,15 +1,15 @@
-# standard modules
+import argparse
+import errno
 import os
 import sys
-import errno
-import argparse
 
 # third party modules
 import fuse
 
 # local modules
-import oscfs.root
 import oscfs.obs
+import oscfs.root
+
 
 class OscFs(fuse.LoggingMixIn, fuse.Operations):
     """The main class implementing the fuse operations and python-fuse
@@ -29,42 +29,42 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
     def _setupParser(self):
 
         self.m_parser = argparse.ArgumentParser(
-            description = "SUSE open build service file system"
+            description="SUSE open build service file system"
         )
 
         self.m_parser.add_argument(
-            "-f", action = 'store_true',
-            help = "Run the file system in the foreground"
+            "-f", action='store_true',
+            help="Run the file system in the foreground"
         )
         self.m_parser.add_argument(
-            "--apiurl", type = str, default = self.m_default_url,
-            help = "The API URL of the OBS instance. openSUSE build service is used by default"
+            "--apiurl", type=str, default=self.m_default_url,
+            help="The API URL of the OBS instance. openSUSE build service is used by default"
         )
         self.m_parser.add_argument(
-            "--homes", action = 'store_true',
-            help = "If set then home projects will be included which is not the default"
+            "--homes", action='store_true',
+            help="If set then home projects will be included which is not the default"
         )
         self.m_parser.add_argument(
-            "--maintenance", action = 'store_true',
-            help = "If set then maintenance projects will be included which is not the default"
+            "--maintenance", action='store_true',
+            help="If set then maintenance projects will be included which is not the default"
         )
         self.m_parser.add_argument(
-            "--ptf", action = 'store_true',
-            help = "If set then PTF projects will be included which is not the default"
+            "--ptf", action='store_true',
+            help="If set then PTF projects will be included which is not the default"
         )
         self.m_parser.add_argument(
-            "mountpoint", type = str,
-            help = "Path where to mount the file system"
+            "mountpoint", type=str,
+            help="Path where to mount the file system"
         )
 
+        CACHE_SECS = oscfs.types.Node.max_cache_time.seconds
+
         self.m_parser.add_argument(
-            "--cache-time", type = int,
-            default = None,
-            help = """Specifies the time in seconds the contents
-                of the file system will be cached. Default: {}
-                seconds. Set to zero to disable caching.""".format(
-                    oscfs.types.Node.max_cache_time.seconds
-            )
+            "--cache-time", type=int,
+            default=None,
+            help=f"""Specifies the time in seconds the contents
+                of the file system will be cached. Default: {CACHE_SECS}
+                seconds. Set to zero to disable caching."""
         )
 
     def _checkAuth(self):
@@ -74,7 +74,7 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
         # exception.
         import urllib.request
         try:
-            prjinfo = self.m_obs.getProjectInfo("openSUSE:Factory")
+            _ = self.m_obs.getProjectInfo("openSUSE:Factory")
             return
         except urllib.request.HTTPError as e:
             if e.code == 401:
@@ -82,7 +82,7 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
                     "Authorization at the remote server failed. Please check your ~/.oscrc user/pass settings for API url {}.".format(
                         self.m_args.apiurl
                     ),
-                    file = sys.stderr
+                    file=sys.stderr
                 )
             elif e.code == 404:
                 # authorization worked but the project is not there, also fine
@@ -93,15 +93,15 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
         except Exception as e:
             print(
                 "Accessing the remote server failed:",
-                e, file = sys.stderr
+                e, file=sys.stderr
             )
             raise
 
         sys.exit(1)
 
-    def _getNode(self, path, fh = None):
+    def _getNode(self, path, fh=None):
 
-        if fh != None:
+        if fh is not None:
             return self._getFileHandle(fh)
         else:
             try:
@@ -114,7 +114,7 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
         self.m_args = self.m_parser.parse_args()
         self.m_obs.configure(self.m_args.apiurl)
         self.m_root = oscfs.root.Root(self.m_obs, self.m_args)
-        if self.m_args.cache_time != None:
+        if self.m_args.cache_time is not None:
             oscfs.types.Node.setMaxCacheTime(self.m_args.cache_time)
 
         self._checkAuth()
@@ -122,13 +122,13 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
         fuse.FUSE(
             self,
             self.m_args.mountpoint,
-            foreground = self.m_args.f,
-            nothreads = True,
+            foreground=self.m_args.f,
+            nothreads=True,
             # direct_io is necessary in our use case to avoid
             # caching in the kernel and support dynamically
             # determined file contents
-            direct_io = True,
-            nonempty = True
+            direct_io=True,
+            nonempty=True
         )
 
     def init(self, path):
@@ -142,7 +142,7 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
 
     # global file system methods
 
-    def getattr(self, path, fh = None):
+    def getattr(self, path, fh=None):
 
         node = self._getNode(path, fh)
 
@@ -150,7 +150,7 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
 
         return ret.toDict()
 
-    def readdir(self, path, fh = None):
+    def readdir(self, path, fh=None):
 
         node = self._getNode(path, fh)
 
@@ -171,7 +171,7 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
 
         fd = self.m_free_handles.pop(0)
 
-        if self.m_handles[fd] != None:
+        if self.m_handles[fd] is not None:
             raise Exception("Handle allocation inconsistency")
 
         self.m_handles[fd] = node
@@ -194,15 +194,15 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
 
     def release(self, path, fh):
 
-        if fh != None:
+        if fh is not None:
             self._freeFileHandle(fh)
 
     def releasedir(self, path, fh):
 
-        if fh != None:
+        if fh is not None:
             self._freeFileHandle(fh)
 
-    def truncate(self, path, length, fh = None):
+    def truncate(self, path, length, fh=None):
 
         node = self._getNode(path, fh)
         if node.getStat().isWriteable():
@@ -239,4 +239,3 @@ class OscFs(fuse.LoggingMixIn, fuse.Operations):
         node = self._getNode(path, fh)
 
         return node.write(data, offset)
-

oscfs/link.py
@@ -1,8 +1,7 @@
-# standard modules
 import os
 
-# local modules
 import oscfs.types
+
 
 class Link(oscfs.types.Node):
     """This type represents a symlink in an OBS package which can
@@ -22,5 +21,3 @@ class Link(oscfs.types.Node):
     def readlink(self):
 
         return self.m_target
-
-

oscfs/misc.py
@@ -1,10 +1,11 @@
-import os, sys
+import os
+import sys
 
 our_uid = None
 our_gid = None
 
-def getUid():
 
+def getUid():
     global our_uid
 
     if our_uid is None:
@@ -12,14 +13,15 @@ def getUid():
 
     return our_uid
 
-def getGid():
 
+def getGid():
     global our_gid
 
     if our_gid is None:
         our_gid = os.getgid()
 
     return our_gid
+
 
 def getFriendlyException(ex):
     """Returns a friendly description of the currently active exception
@@ -28,11 +30,11 @@ def getFriendlyException(ex):
     import traceback
 
     _, _, tb = sys.exc_info()
-    fn, ln, _, _ = frame = traceback.extract_tb(tb)[-1]
+    fn, ln, _, _ = traceback.extract_tb(tb)[-1]
     return "Exception in {}:{}: {}".format(fn, ln, str(ex))
+
 
 def printException(ex):
     """Prints the currently active exception in a friendly, compact
     way to stderr."""
-    print(getFriendlyException(ex), file = sys.stderr)
-
+    print(getFriendlyException(ex), file=sys.stderr)

commit 609eba9bc827a320270d4f80176d08e1c97c0baa

fix regressions introduced by commit cd692a1d2f9fabe4ac5451832c3b7df109302c07
The fixing of flake8 findings caused a logic inversion bug in the
isRoot() method. This causes symlinks to be broken (missing relative
path components). Fix that.

Also for the `commits` sub-directory a format string prefix is missing
that broke in the same commit.

oscfs/package.py
@@ -70,7 +70,6 @@ class Package(oscfs.types.DirNode):
                     revision=self.m_revision
                 )
             elif ft == types.symlink:
-                target = "../../" + target
                 node = oscfs.link.Link(self, name, target)
             else:
                 raise Exception("Unexpected type")
@@ -210,7 +209,7 @@ class CommitsDir(oscfs.types.DirNode):
         for rev in range(len(infos)):
             info = infos[rev]
 
-            commit = "{rev + 1}"
+            commit = f"{rev + 1}"
             self.m_entries[commit] = CommitNode(self, info, commit)
 
 

oscfs/types.py
@@ -189,7 +189,7 @@ class Node:
     def isRoot(self):
         """Returns whether this node is the root node of the file
         system."""
-        return self.m_parent is not None
+        return self.m_parent is None
 
     def getRoot(self):
         """Returns the root node of the file system."""